96286f274ef6270b8df018bb2e4308c67c4cbed5
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-14 11:46:46 -0500

    Classes1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Classes
+﻿using System;
+using System.Reflection;
+using System;
+
+namespace Application
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            public class Car
+         {
+            public int Age = 2022;
+            public string Make { get; set; }
+            public string Model { get; set; }
+
+            public void HowOldCarIs()
+            {
+                Console.WriteLine($"Year of the Jeep Wrangler is {Age}");
+            }
+
+            Car c2 = new Car()
+            {
+                Make = "Jeep",
+                Model = "Wrangler",
+            }
         }
     }
 }
+        
+    //Create a Make property of type string that is public
+    //Create a Model property of type string that is public
+    //Create a Year property of type int that is publiC 
+
+    
