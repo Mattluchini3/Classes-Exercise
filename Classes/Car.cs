2eec668cf1566729830f24710d389b00db2f2d90
Matthew Luchini <luch@Matthews-MBP.lan>
2023-02-20 20:34:45 -0500

    Tryingagain

---
+++ Classes/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+        public int Age = 2022;
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
+
