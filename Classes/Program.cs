3db2bd0719295aa54bd848849e92e53b68a56136
Matthew Luchini <luch@Matthews-MBP.lan>
2023-03-13 22:18:04 -0400

    Finished

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -7,29 +7,17 @@ namespace Classes
     {
         static void Main(string[] args)
         {
-              public class Car
-        {
-            public string Make { get; set; }
-            public string Model { get; set; }
-            public int Year { get; set; }
+            Car car1 = new Car();
+            car1.Make = "Jeep";
+            car1.Model = "Wrangler";
+            car1.Year = 2015;
 
-
+            Console.WriteLine($"The make of the car: {car1.Make}.");
+            Console.WriteLine($"The model of the car: {car1.Model}.");
+            Console.WriteLine($"The year of the car: {car1.Year}.");
         }
-        public void HowOldCarIs()
-        {
-            Console.WriteLine($"Year of {myCar.Make} {myCar.Model} is {myCar.Year}");
-        }
-        Car myCar = new Car()
-        {
-            Make = "Jeep",
-            Model = "Wrangler",
-            Year = 2020,
-        };
-    } 
+    }
 }
-
-
-
 
 
 //Create a Make property of type string that is public
